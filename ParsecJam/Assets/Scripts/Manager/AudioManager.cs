ae79567ce59c3411e96235a0eba7ea574bc3e173
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-11-02 12:23:57 +0100

    [FEAT] Main prefabs (UI + Managers) with scripts set up

---
+++ ParsecJam/Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+    
+    public SoundEffect[] soundEffects;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+
+        foreach (SoundEffect fx in soundEffects)
+        {
+            fx.source = gameObject.AddComponent<AudioSource>();
+            fx.source.volume = fx.volume;
+            fx.source.priority = fx.priority;
+            fx.source.loop = fx.loop;
+        }
+    }
+
+    private void Start()
+    {
+        Play("Music");
+    }
+
+    public void Play(string name)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+            return;
+        }
+        fx.source.clip = fx.clip[UnityEngine.Random.Range(0, fx.clip.Length)];
+        fx.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+            return;
+        }
+        fx.source.Stop();
+    }
+    
+    public float GetClipLength(string name)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == name);
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+            return 0;
+        }
+        return fx.source.clip.length;
+    }
+
+    public void SpeedMusicUp(float pitchWanted)
+    {
+        StartCoroutine(IncreasePitch(pitchWanted));
+    }
+
+    public void SlowMusicDown(float pitchWanted)
+    {
+        StartCoroutine(DecreasePitch(pitchWanted));
+    }
+
+    private IEnumerator IncreasePitch(float pitchWanted)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == "Music");
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+        }
+        if (fx.source.pitch + 0.05f <= pitchWanted)
+        {
+            fx.source.pitch += 0.05f;
+            yield return new WaitForSeconds(0.3f);
+            StartCoroutine(IncreasePitch(pitchWanted));
+        }
+        else
+        {
+            fx.source.pitch = pitchWanted;
+        }
+    }
+
+    private IEnumerator DecreasePitch(float pitchWanted)
+    {
+        SoundEffect fx = Array.Find(soundEffects, sound => sound.clipName == "Music");
+        if (fx == null)
+        {
+            Debug.Log("/!\\ Sound : " + name + "not found /!\\");
+        }
+        if (fx.source.pitch - 0.05f >= pitchWanted)
+        {
+            fx.source.pitch -= 0.05f;
+            yield return new WaitForSeconds(0.3f);
+            StartCoroutine(DecreasePitch(pitchWanted));
+        }
+        else
+        {
+            fx.source.pitch = pitchWanted;
+        }
+    }
+}
---
+++ ParsecJam/Assets/Scripts/Manager/InterfaceManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour
+{
+
+    [SerializeField] private GameObject _gamePanel;
+    [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private GameObject _winPanel;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameManager.instance.onStateChange += () => {
+            if (GameManager.instance.state == State.PAUSE)
+            {
+                GoInpause();
+            }
+            else if (GameManager.instance.state == State.INGAME)
+            {
+                GoToGame();
+            }
+            else if (GameManager.instance.state == State.WIN)
+            {
+                GoToWin();
+            }
+        };
+        GoToGame();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GoInpause();
+        }
+    }
+
+    public void GoInpause()
+    {
+        Time.timeScale = 0;
+        _gamePanel.SetActive(false);
+        _pausePanel.SetActive(true);
+    }
+
+    private void GoToGame()
+    {
+        _pausePanel.SetActive(false);
+        _winPanel.SetActive(false);
+        _gamePanel.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    private void GoToWin()
+    {
+        _gamePanel.SetActive(false);
+        _pausePanel.SetActive(false);
+        _winPanel.SetActive(true);
+    }
+
+    #region Button Functions
+
+    public void Resume()
+    {
+        GameManager.instance.ChangeState(State.INGAME);
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    #endregion
+}

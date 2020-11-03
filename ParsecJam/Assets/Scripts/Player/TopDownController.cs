48f875581f8d1aa2653e9ca29b851c825743702c
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-11-03 08:35:59 +0100

    [FEAT] Player 2 added to scene

--- ParsecJam/Assets/Scripts/Player/TopDownController.cs
+++ ParsecJam/Assets/Scripts/Player/TopDownController.cs
@@ -5,10 +5,10 @@ using Rewired;
 
 public class TopDownController : MonoBehaviour
 {
-    public TopDownEntity entity;
-    private Player _rewiredPlayer;
+    public TopDownEntity[] players;
+    private Player[] _rewiredPlayer;
 
-    private Vector2 aimMoveDir;
+    private Vector2[] _aimMoveDir;
 
     private bool _isPlaying;
 
@@ -28,9 +28,12 @@ public class TopDownController : MonoBehaviour
             }
         };
 
-        _rewiredPlayer = ReInput.players.GetPlayer("Player1");
-
-
+        _rewiredPlayer = new Player[players.Length];
+        for (int i = 0; i < players.Length; i++)
+        {
+            _rewiredPlayer[i] = ReInput.players.GetPlayer("Player" + (i+1).ToString());
+        }
+        _aimMoveDir = new Vector2[players.Length];
 
         _isPlaying = true;
     }
@@ -40,11 +43,12 @@ public class TopDownController : MonoBehaviour
     {
         if (_isPlaying)
         {
-            float dirX = _rewiredPlayer.GetAxis("MoveX");
-            float dirY = _rewiredPlayer.GetAxis("MoveY");
+            #region Player1
+            float dirX = _rewiredPlayer[0].GetAxis("MoveX");
+            float dirY = _rewiredPlayer[0].GetAxis("MoveY");
 
-            float aimDirX = _rewiredPlayer.GetAxis("AimX");
-            float aimDirY = _rewiredPlayer.GetAxis("AimY");
+            float aimDirX = _rewiredPlayer[0].GetAxis("AimX");
+            float aimDirY = _rewiredPlayer[0].GetAxis("AimY");
 
             Vector2 moveDir = new Vector2(dirX, dirY);
             moveDir.Normalize();
@@ -52,24 +56,59 @@ public class TopDownController : MonoBehaviour
 
             if (aimDirX != 0 && aimDirY != 0)
             {
-                aimMoveDir = new Vector2(aimDirX, aimDirY);
-                aimMoveDir.Normalize();
+                _aimMoveDir[0] = new Vector2(aimDirX, aimDirY);
+                _aimMoveDir[0].Normalize();
             }
 
 
-            entity.Move(moveDir, aimMoveDir);
+            players[0].Move(moveDir, _aimMoveDir[0]);
 
-            if (_rewiredPlayer.GetButtonDown("Dash"))
+            if (_rewiredPlayer[0].GetButtonDown("Dash"))
             {
-                entity.Dash();
+                players[0].Dash();
             }
-            if (_rewiredPlayer.GetButton("Shoot"))
+            if (_rewiredPlayer[0].GetButton("Shoot"))
             {
-                entity.shootFunc.ShootBullet();
+                players[0].shootFunc.ShootBullet();
             }
+            #endregion
+
+            #region Player2
+            dirX = _rewiredPlayer[1].GetAxis("MoveX");
+            dirY = _rewiredPlayer[1].GetAxis("MoveY");
+
+            aimDirX = _rewiredPlayer[1].GetAxis("AimX");
+            aimDirY = _rewiredPlayer[1].GetAxis("AimY");
+
+            moveDir = new Vector2(dirX, dirY);
+            moveDir.Normalize();
+
+
+            if (aimDirX != 0 && aimDirY != 0)
+            {
+                _aimMoveDir[1] = new Vector2(aimDirX, aimDirY);
+                _aimMoveDir[1].Normalize();
+            }
+
+
+            players[1].Move(moveDir, _aimMoveDir[1]);
+
+            if (_rewiredPlayer[1].GetButtonDown("Dash"))
+            {
+                players[1].Dash();
+            }
+            if (_rewiredPlayer[1].GetButton("Shoot"))
+            {
+                players[1].shootFunc.ShootBullet();
+            }
+            #endregion
         }
 
-        if (_rewiredPlayer.GetButtonDown("Pause"))
+        if (_rewiredPlayer[0].GetButtonDown("Pause"))
+        {
+            GameManager.instance.Pause();
+        }
+        if (_rewiredPlayer[1].GetButtonDown("Pause"))
         {
             GameManager.instance.Pause();
         }

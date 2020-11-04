cdec0d730cf34b95eedf626374c4afa1282408fe
Grégoire <gpierillas@gmail.com>
2020-11-04 12:58:55 +0100

    [Feat] eyes prefab and scripts

---
+++ ParsecJam/Assets/Scripts/Eyes/Eyes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour
+{
+    private Animator _anim;
+    
+    [SerializeField] float _randomMin;
+    [SerializeField] float _randomMax;
+
+    private float randomEyeOpening = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+
+        randomEyeOpening = Random.Range(_randomMin, _randomMax);
+        StartCoroutine(EyeOpening());
+    }
+
+    private IEnumerator EyeOpening()
+    {
+        yield return new WaitForSeconds(randomEyeOpening);
+        OpenEye();
+    }
+
+    
+    public void OpenEye()
+    {
+        _anim.SetTrigger("OpenEye");
+    }
+    
+    public void CloseEye()
+    {
+        _anim.SetTrigger("CloseEye");
+    }
+}
---
+++ ParsecJam/Assets/Scripts/Eyes/EyesFollow.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class EyesFollow : MonoBehaviour
+{
+
+    [SerializeField] private float _timerBetweenChecksMax = 3;
+    [SerializeField] private Transform _orbite;
+    private float _timerBetweenChecks = 3;
+
+    private bool _isDoneChecking = false;
+
+    [Header("Player")]
+    [SerializeField] private float _distanceMinToFollowPlayer = 3;
+    private Transform _currentPlayerTarget;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timerBetweenChecks = _timerBetweenChecksMax;
+        //square it for distance check
+        _distanceMinToFollowPlayer *= _distanceMinToFollowPlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!_isDoneChecking)
+            _timerBetweenChecks -= Time.deltaTime;
+
+        if (_timerBetweenChecks < 0)
+        {
+            _timerBetweenChecks = _timerBetweenChecksMax;
+            _isDoneChecking = true;
+        }
+
+        if (_isDoneChecking)
+        {
+            
+            TopDownEntity[] players = LevelManager.instance.players;
+
+            float distance = CalculateDistance(players[0].transform);
+            float distance2 = CalculateDistance(players[1].transform);
+
+            if (distance < _distanceMinToFollowPlayer && distance2 < _distanceMinToFollowPlayer)
+            {
+                if (distance < distance2)
+                    _currentPlayerTarget = players[0].transform;
+                else
+                    _currentPlayerTarget = players[1].transform;
+            }
+            else if (distance < _distanceMinToFollowPlayer)
+            {
+                _currentPlayerTarget = players[0].transform;
+            }
+            else if (distance2 < _distanceMinToFollowPlayer)
+            {
+                _currentPlayerTarget = players[1].transform;
+            }
+            else
+            {
+                _currentPlayerTarget = null;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_currentPlayerTarget == null) return;
+        
+        _orbite.LookAt(new Vector3(_currentPlayerTarget.position.x, _currentPlayerTarget.position.y + 1.5f, _currentPlayerTarget.position.z));
+    }
+
+    private float CalculateDistance(Transform targetTransform)
+    {
+        return ((Vector3)targetTransform.position - (Vector3)transform.position).sqrMagnitude;
+    }
+}

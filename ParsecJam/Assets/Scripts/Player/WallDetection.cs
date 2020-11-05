16616ff9747f862e4ce04bc0f4aa4a05345bdec0
Matilde <matilde.rodrigues@edu.devinci.fr>
2020-11-05 11:54:05 +0100

    [FIX] PLayer can't go through walls

---
+++ ParsecJam/Assets/Scripts/Player/WallDetection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetection : MonoBehaviour
+{
+
+    private TopDownEntity _entity;
+    [SerializeField] private WallDetectionTrigger _detectionTrigger;
+    private Vector3 _contactPoint;
+    private bool _isOutOfCollision = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _entity = GetComponent<TopDownEntity>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(!_isOutOfCollision && _detectionTrigger.GetIsInWall())
+        {
+            _isOutOfCollision = true;
+            _entity.transform.position = _entity.transform.position + _contactPoint * 2;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            _isOutOfCollision = true;
+        }
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            _isOutOfCollision = false;
+            _contactPoint = -collision.contacts[0].normal;
+        }
+    }
+}
---
+++ ParsecJam/Assets/Scripts/Player/WallDetectionTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallDetectionTrigger : MonoBehaviour
+{
+    private bool _isInWall = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            print("miaou");
+
+            _isInWall = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            print("miaou");
+
+            _isInWall = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            _isInWall = false;
+        }
+    }
+
+    public bool GetIsInWall()
+    {
+        return _isInWall;
+    }
+}
